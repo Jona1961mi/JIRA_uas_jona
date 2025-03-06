985d1411774eaa9f57fd5acdfe53fd117cd6525e
Jona1961mi <jonathancansinoperez@gmail.com>
2025-03-05 20:28:51 -0700

    ejercicios

---
+++ ejercicio 2.py
@@ -0,0 +1,4 @@
+def suma (a,b):
+    return a+b
+resultado =suma(2,1)
+print (resultado)
---
+++ ejercicio 3.py
@@ -0,0 +1,10 @@
+#funciones 
+al_cuadrado=lambda x:x**2
+print("funcion lambda")
+print(al_cuadrado(2))
+
+def area_rect(base,altura):
+    return base*altura
+resultado=area_rect(2,3)
+print("funcion de rectangulo")
+print(resultado)
