42bf4b8df690a4792115d875d28803d2944a82e9
Jona1961mi <jonathancansinoperez@gmail.com>
2025-09-04 22:00:40 -0700

    Update matriz.py

--- Matriz_rotacional/matriz.py
+++ Matriz_rotacional/matriz.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 def rotacion_3d(punto, theta, eje='z'):
     if eje == 'x':
@@ -25,21 +24,6 @@ rotado_y = rotacion_3d(p, np.pi/2, eje='y')
 # Rotar 90° sobre Z
 rotado_z = rotacion_3d(p, np.pi/2, eje='z')
 
-# Dibujar
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-# Origen
-ax.quiver(0, 0, 0, p[0], p[1], p[2], color='r', label="Original")
-ax.quiver(0, 0, 0, rotado_y[0], rotado_y[1], rotado_y[2], color='g', label="Rotado eje Y")
-ax.quiver(0, 0, 0, rotado_z[0], rotado_z[1], rotado_z[2], color='b', label="Rotado eje Z")
-
-# Configuración gráfica
-ax.set_xlim([-1.5, 1.5])
-ax.set_ylim([-1.5, 1.5])
-ax.set_zlim([-1.5, 1.5])
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-ax.legend()
-plt.show()
+print("Punto original:", p)
+print("Rotado 90° sobre eje Y:", rotado_y)
+print("Rotado 90° sobre eje Z:", rotado_z)
